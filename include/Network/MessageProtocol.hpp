3dc73ce396f4ad0b3c1843a20eca7cfa894fe5dd
PierrePruvost03 <pierre.pruvost@epitech.eu>
2026-01-10 10:49:32 +0100

    style: remove too long lines, missing include, wrong indentation

--- include/Network/MessageProtocol.hpp
+++ include/Network/MessageProtocol.hpp
@@ -1,11 +1,14 @@
 #pragma once
+
 #include <stdexcept>
+#include <vector>
+
 #include "Network/ProtocolManager.hpp"
 
 namespace net {
 
 class MessageProtocol {
-  public:
+ public:
     /**
      * @brief Constructs a MessageProtocol object.
      *
@@ -55,11 +58,12 @@ class MessageProtocol {
         return T::deserialize(unformatted.data);
     }
 
-  private:
+ private:
     ProtocolManager& _protocolManager;
 
     uint32_t readMessageId(const std::vector<uint8_t>& data) {
-        if (_protocolManager.getEndianness() == ProtocolManager::Endianness::BIG) {
+        if (_protocolManager.getEndianness()
+            == ProtocolManager::Endianness::BIG) {
             return (static_cast<uint32_t>(data[0]) << 24) |
                    (static_cast<uint32_t>(data[1]) << 16) |
                    (static_cast<uint32_t>(data[2]) << 8) |
